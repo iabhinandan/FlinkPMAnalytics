aa1c8ba59d52032f75bae0cfb9da3ff2e525de1d
Abhinandan Chakraborty <chakraborty.abhinandan@gmail.com>
2025-03-14 23:13:37 +0100

    Add build statement

--- src/main/java/org/abhinandan/flink/Main.java
+++ src/main/java/org/abhinandan/flink/Main.java
@@ -1,7 +1,24 @@
 package org.abhinandan.flink;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        // Create Flink execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+
+        // Create a simple DataStream
+        DataStream<String> text = env.fromElements("Hello", "Flink", "World!");
+
+        // Process DataStream
+        DataStream<String> result = text.map((MapFunction<String, String>) value -> "Processed: " + value).rebalance();
+
+        // Print the output
+        result.print();
+
+        // Execute Flink job
+        env.execute("Flink Hello World");
     }
 }
